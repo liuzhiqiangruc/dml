ec60843bf130cefc38a14760f529c5ceb06324a7
liuzhiqiang <liuzhiqiang@MacBook-Pro.local>
2016-07-15 16:55:36 +0800

    add
    Change-Id: I9c80167c1d6e1307854f70c32606ed4436f30f83

---
+++ tm/tm.h
@@ -0,0 +1,24 @@
+/* ========================================================
+ *   Copyright (C) 2016 All rights reserved.
+ *   
+ *   filename : tm.h
+ *   author   : liuzhiqiangruc@126.com
+ *   date     : 2016-07-15
+ *   info     : 
+ * ======================================================== */
+
+#ifndef _TM_H
+#define _TM_H
+
+#define KEY_SIZE 64
+#define LDA_LINE_LEN 1024
+
+typedef struct _tm TM;
+
+TM * tm_create(int argc, char *argv[]);
+int  tm_init(TM * tm);
+int  tm_est (TM * tm);
+void tm_save(TM * tm, int n);
+void tm_free(TM * tm);
+
+#endif //TM_H
---
+++ tm/tm_config_h.h
@@ -0,0 +1,39 @@
+/* ========================================================
+ *   Copyright (C) 2016 All rights reserved.
+ *   
+ *   filename : tm_config_h.h
+ *   author   : liuzhiqiangruc@126.com
+ *   date     : 2016-07-15
+ *   info     : 
+ * ======================================================== */
+
+#ifndef _TM_CONFIG_H_H
+#define _TM_CONFIG_H_H
+
+#include "tm_config.h"
+
+typedef struct _tmconfigh TMHConfig;
+
+typedef double (*GET_G0)(void *);
+typedef double (*GET_G1)(void *);
+
+struct _tmconfigh{
+    double g0;
+    double g1;
+    TMConfig * tmc;
+    GET_ALPHA  get_alpha;
+    GET_BETA   get_beta;
+    GET_K      get_k;
+    GET_N      get_n;
+    GET_S      get_s;
+    GET_D      get_d;
+    GET_O      get_o;
+    GET_G0     get_g0;
+    GET_G1     get_g1;
+    SET_CONFIG set;
+};
+
+
+TMHConfig * init_h_config();
+
+#endif //TM_CONFIG_H_H

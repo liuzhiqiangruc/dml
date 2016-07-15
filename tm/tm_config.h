3cdef2a28ef7f00ebdd9127164cb803f7920b63c
liuzhiqiang <liuzhiqiang@MacBook-Pro.local>
2016-07-15 16:55:19 +0800

    add tm_config
    Change-Id: Iae918df1e4de5caa81f344fff27a1bff7e3dfbcd

---
+++ tm/tm_config.h
@@ -0,0 +1,44 @@
+/* ========================================================
+ *   Copyright (C) 2016 All rights reserved.
+ *   
+ *   filename : tm_config.h
+ *   author   : liuzhiqiangruc@126.com
+ *   date     : 2016-07-15
+ *   info     : 
+ * ======================================================== */
+
+#ifndef _TM_CONFIG_H
+#define _TM_CONFIG_H
+
+typedef void   (*SET_CONFIG)(void * tmc, int argc, char * argv[]);
+typedef double (*GET_ALPHA) (void * tmc);
+typedef double (*GET_BETA)  (void * tmc);
+typedef int    (*GET_K)     (void * tmc);
+typedef int    (*GET_N)     (void * tmc);
+typedef int    (*GET_S)     (void * tmc);
+typedef char * (*GET_D)     (void * tmc);
+typedef char * (*GET_O)     (void * tmc);
+
+typedef struct _tmconfig{
+    double a;
+    double b;
+    int    k;
+    int    n;
+    int    s;
+    char * d;
+    char * o;
+    GET_ALPHA  get_alpha;
+    GET_BETA   get_beta;
+    GET_K      get_k;
+    GET_N      get_n;
+    GET_S      get_s;
+    GET_D      get_d;
+    GET_O      get_o;
+    SET_CONFIG set;
+} TMConfig;
+
+
+TMConfig * init_config();
+void       free_config(TMConfig * tmc);
+
+#endif //TM_CONFIG_H

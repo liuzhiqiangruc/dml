5d25a4cf7e6c1871487c9308152049d8ad794e22
Liu <liuzhiqiangruc@126.com>
2016-12-08 19:19:49 +0800

    rnnlm code done

--- w2v/rnnlm.c
+++ w2v/rnnlm.c
@@ -66,7 +66,7 @@ int rnn_init  (RNNLM * rnnlm){
     return 0;
 }
 
-static double * forward_st (RNNLM * rnnlm, int id, int sid, int w, int k){
+static inline double * forward_st (RNNLM * rnnlm, int id, int sid, int w, int k){
 
     double *in, *ls, *st;
     int i, j, tid, lsid;
@@ -91,11 +91,9 @@ static double * forward_st (RNNLM * rnnlm, int id, int sid, int w, int k){
     return st;
 }
 
-static void accumulate_grad_w (RNNLM * rnnlm, double * sg, double * wg, int lsid, int w, int k){
+static inline void accumulate_grad_w (RNNLM * rnnlm, double * sg, double * wg, int lsid, int k){
     double *ls;
     int i, j;
-
-    lsid = lsid % w;
 
     ls = rnnlm->rnn->s + lsid * k;
 
@@ -106,7 +104,7 @@ static void accumulate_grad_w (RNNLM * rnnlm, double * sg, double * wg, int lsid
     }
 }
 
-static void update_input_u (RNNLM * rnnlm, double * sg, double alpha, int id, int k){
+static inline void update_input_u (RNNLM * rnnlm, double * sg, double alpha, int id, int k){
     int tid, i;
     double * in;
 
@@ -118,7 +116,7 @@ static void update_input_u (RNNLM * rnnlm, double * sg, double alpha, int id, in
     }
 }
 
-static void back_propgation_s (RNNLM * rnnlm, double * sg, double *tg, int lsid, int k) {
+static inline void back_propgation_s (RNNLM * rnnlm, double * sg, double *tg, int lsid, int k) {
     int i, j;
     double *ls;
     ls = rnnlm->rnn->s + lsid * k;
@@ -127,7 +125,7 @@ static void back_propgation_s (RNNLM * rnnlm, double * sg, double *tg, int lsid,
 
     for (i = 0; i < k; i++){
         for (j = 0; j < k; j++){
-            tg[i] += rnnlm->rnn->w[i * k + j] * sg[j];
+            tg[i] += rnnlm->rnn->w[j * k + i] * sg[j];
         }
         tg[i] *= ls[i] * (1.0 - ls[i]);
     }
@@ -135,7 +133,7 @@ static void back_propgation_s (RNNLM * rnnlm, double * sg, double *tg, int lsid,
     memcpy(sg, tg, sizeof(double) * k);
 }
 
-static void update_w (RNNLM * rnnlm, double * wg, int k, double alpha){
+static inline void update_w (RNNLM * rnnlm, double * wg, int k, double alpha){
     int i, j;
     for (i = 0; i < k; i++){
         for(j = 0; j < k; j++){
@@ -146,68 +144,46 @@ static void update_w (RNNLM * rnnlm, double * wg, int k, double alpha){
 
 
 void rnn_learn(RNNLM * rnnlm){
+    double *st, *sg, *tg, *wg, alpha;
+    int i, k, n, w, d, id, ds, de, sid, lsid;
 
-    double * st = NULL;    // current input of output layer
-    double * sg = NULL;    // gradient of st
-    double * tg = NULL;    // temp version for sg
-    double * wg = NULL;    // gradient of rnn->w
-
-    double alpha;
-
-    int i, k;
-    int n, w;
-    int d, id, ds, de;
-    int sid, lsid;
-
-    n = rnnlm->rc->get_n(rnnlm->rc);
-    k = rnnlm->rc->get_k(rnnlm->rc);
-    w = rnnlm->rc->get_w(rnnlm->rc);
-
+    n     = rnnlm->rc->get_n(rnnlm->rc);
+    k     = rnnlm->rc->get_k(rnnlm->rc);
+    w     = rnnlm->rc->get_w(rnnlm->rc);
     alpha = rnnlm->rc->get_alpha(rnnlm->rc);
-
-    tg = (double*)malloc(sizeof(double) * k);
-    sg = (double*)malloc(sizeof(double) * k);
-    wg = (double*)malloc(sizeof(double) * k * k);
+    tg    = (double*)malloc(sizeof(double) * k);
+    sg    = (double*)malloc(sizeof(double) * k);
+    wg    = (double*)malloc(sizeof(double) * k * k);
 
     while (n-- > 0) for (d = 0; d < rnnlm->ds->d; d++){
-
         sid = 0;   // first token of current doc | sentence
         ds = rnnlm->ds->doffs[d];         // begin pos
         de = rnnlm->ds->doffs[d + 1];     // end   pos
         memset(wg, 0, sizeof(double) * k * k);
-
-        // if doc length > 1
         if (de - ds > 1) for (id = ds; id < de - 1; id++){
-
-            // current st : output of hidden layer
             st = forward_st (rnnlm, id, sid, w, k);
 
-            // update V and get sg for st
             memset(sg, 0, sizeof(double) * k);
             hsoft_learn(rnnlm->hsf, st, sg, rnnlm->ds->tokens[id + 1], alpha);
-
-            //  update gradient for through down
             for (i = 0; i < k; i++){
-                sg[i] *= st[i] * (1 - st[i]);
+                sg[i] *= st[i] * (1.0 - st[i]);
             }
-
-            // update input
             update_input_u (rnnlm, sg, alpha, id, k);
 
-            // back propagation for w step most
-            for (i = 1; i < w; i++) if ((lsid = sid - i) >= 0){
-                // w gradient
-                accumulate_grad_w (rnnlm, sg, wg, lsid, w, k);
-
-                // gradient back propgation for last
-                back_propgation_s (rnnlm, sg, tg, lsid % w, k);
-
-                // update last input
-                update_input_u (rnnlm, sg, alpha, id - i, k);
+            for (i = 1; i < w; i++) if ((lsid = sid - i) >= 0) {
+                lsid = lsid % w;
+                accumulate_grad_w (rnnlm, sg, wg, lsid, k);
+                back_propgation_s (rnnlm, sg, tg, lsid, k);
+                update_input_u    (rnnlm, sg, alpha, id - i, k);
             }
             sid += 1;
         }
-        // update w once for a sentence
-        update_w(rnnlm, wg, k, alpha);
+        if (de - ds > 1) {
+            update_w(rnnlm, wg, k, alpha);
+        }
     }
+
+    free(sg);      sg = NULL;
+    free(tg);      tg = NULL;
+    free(wg);      wg = NULL;
 }
